3f32c76af09bc753e35096bcc2e698e3a9289bd6
chiayinin <chiayinin@gmail.com>
2022-04-03 23:38:24 +0800

    gh-page

---
+++ src/views/FrontNavber.vue
@@ -0,0 +1,24 @@
+<template>
+  <Navbar></Navbar>
+  <div class="container">
+    <div class="row">
+      <Carousel></Carousel>
+    </div>
+  </div>
+  <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/FrontNavbar.vue';
+import Footer from '@/components/FrontFooter.vue';
+import Carousel from '@/components/FrontCarousel.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+    Carousel
+  }
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  publicPath: process.env.NODE_ENV === 'production' ? '/artist-studio/' : '/'
+  transpileDependencies: true
 });
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/artist-studio/' : '/'
+};
